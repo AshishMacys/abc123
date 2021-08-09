e533402042d47e27536fda6fc792668eabe8ac25
Ashish Singh <ashish.dit@gmail.com>
2021-08-09 12:29:35 -0400

    20210809-002

--- Integrations_submit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-system_pw=`cat oic_pwd`
-curl -G -X GET -u ashish.singh2@globalpay.com:${system_pw} -H "Accept:application/json" -d "expand=connection" https://icsprod-gpics.uscom-central-1.oraclecloud.com/ic/api/integration/v1/integrations -o gen1.json
-curl -G -X GET -u ashish.singh2@globalpay.com:${system_pw} -H "Accept:application/json" -d "expand=connection" https://oicprod1-gpics-ia.integration.ocp.oraclecloud.com/ic/api/integration/v1/integrations -o gen21.json
-curl -G -X GET -u ashish.singh2@globalpay.com:${system_pw} -H "Accept:application/json" -d "expand=connection" https://oicprod2-gpics-ia.integration.ocp.oraclecloud.com/ic/api/integration/v1/integrations -o gen22.json
